dca067eaeccdf47db28240034ae8f28f2d42d397
Harry Grenier <hgrenier@uri.edu>
2024-11-07 13:06:46 -0500

    working

--- huffman.py
+++ huffman.py
@@ -10,6 +10,7 @@ except ImportError:
     import pickle
 
 def code(msg):
+    """Build a Huffman tree and encode the message."""
     freqs = {}
     for char in msg:
         freqs[char] = freqs.get(char, 0) + 1
@@ -24,25 +25,30 @@ def code(msg):
         for pair in hi[1:]:
             pair[1] = '1' + pair[1]
         heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    huffman_tree = {char: code for char, code in heap[0][1:]}
 
+    huffman_tree = {char: code for char, code in heap[0][1:]}
     encoded_msg = ''.join(huffman_tree[char] for char in msg)
     return encoded_msg, huffman_tree
 
+
+
 def decode(encoded_str, tree):
+    """Decode a binary-encoded string using the Huffman tree."""
     reverse_tree = {v: k for k, v in tree.items()}
-    decoded_msg = ""
+    decoded_msg = bytearray()
     current_code = ""
     for bit in encoded_str:
         current_code += bit
         if current_code in reverse_tree:
-            decoded_msg += reverse_tree[current_code]
+            decoded_msg.append(reverse_tree[current_code])
             current_code = ""
-    return decoded_msg
+    return bytes(decoded_msg)
+
 
 def compress(msg):
+    """Compress the message and return binary compressed data."""
     encoded_msg, tree = code(msg)
-    padding = 8 - len(encoded_msg) % 8
+    padding = (8 - len(encoded_msg) % 8) % 8
     encoded_msg += '0' * padding
 
     compressed_data = array.array('B')
@@ -53,16 +59,21 @@ def compress(msg):
     tree['pad'] = padding
     return compressed_data, tree
 
+
+
 def decompress(bitstring, tree):
-    padding = tree.pop('pad', 0)  # Retrieve padding information
+    """Decompress the binary bitstring using the Huffman tree."""
+    padding = tree.pop('pad', 0)
     bit_str = ''.join(f"{byte:08b}" for byte in bitstring)[:-padding]
     return decode(bit_str, tree)
+
+
 
 def usage():
     sys.stderr.write("Usage: {} [-c|-d|-v|-w] infile outfile\n".format(sys.argv[0]))
     exit(1)
 
-if __name__ == '__main__':
+if __name__=='__main__':
     if len(sys.argv) != 4:
         usage()
     opt = sys.argv[1]
@@ -86,27 +97,30 @@ if __name__ == '__main__':
     assert os.path.exists(infile)
 
     if compressing or encoding:
-        with open(infile, 'rb') as fp:
-            msg = fp.read().decode('utf-8')
+        fp = open(infile, 'rb')
+        msg = fp.read()
+        fp.close()
         if compressing:
-            compressed_data, tree = compress(msg)
-            with open(outfile, 'wb') as fcompressed:
-                marshal.dump((pickle.dumps(tree), compressed_data), fcompressed)
+            compr, tree = compress(msg)
+            fcompressed = open(outfile, 'wb')
+            marshal.dump((pickle.dumps(tree), compr), fcompressed)
+            fcompressed.close()
         else:
-            encoded_msg, tree = code(msg)
-            print(encoded_msg)
-            with open(outfile, 'wb') as fcompressed:
-                marshal.dump((pickle.dumps(tree), encoded_msg), fcompressed)
+            enc, tree = code(msg)
+            print(msg)
+            fcompressed = open(outfile, 'wb')
+            marshal.dump((pickle.dumps(tree), enc), fcompressed)
+            fcompressed.close()
     else:
-        with open(infile, 'rb') as fp:
-            pickled_tree, data = marshal.load(fp)
-            tree = pickle.loads(pickled_tree)
+        fp = open(infile, 'rb')
+        pickleRick, compr = marshal.load(fp)
+        tree = pickle.loads(pickleRick)
+        fp.close()
         if decompressing:
-            msg = decompress(data, tree)
-            with open(outfile, 'w') as fdecompressed:
-                fdecompressed.write(msg)
+            msg = decompress(compr, tree)
         else:
-            decoded_msg = decode(data, tree)
-            print(decoded_msg)
-            with open(outfile, 'w') as fdecoded:
-                fdecoded.write(decoded_msg)
+            msg = decode(compr, tree)
+            print(msg)
+        fp = open(outfile, 'wb')
+        fp.write(msg)
+        fp.close()
